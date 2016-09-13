4deb908b00075fa198de2aab16015296b8d0f922
Maldon <maldonmeehan@gmail.com>
2016-09-13 14:05:39 -0700

    add custom routes for login, logout and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   root :to => 'lists#index'
 
   resources :lists do
