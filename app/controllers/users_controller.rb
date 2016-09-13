00a0143319ce2b8ccdf7042bae0dfad628cd376a
Maldon <maldonmeehan@gmail.com>
2016-09-13 10:12:43 -0700

    add create function for user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Thank you for signing up!"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'lists#index'
-  
+
   resources :lists do
     resources :tasks, :except => [:show, :index]
   end
+
+  resources :users
 end
